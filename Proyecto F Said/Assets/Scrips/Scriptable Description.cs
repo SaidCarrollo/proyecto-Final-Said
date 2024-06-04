c35d7ee3ce6dd91018e52547278b2ff260030386
ArielCarrollo <150464222+ArielCarrollo@users.noreply.github.com>
2024-06-03 23:39:00 -0500

    Avance camara y colisiones
    Commit 3

---
+++ Proyecto F Said/Assets/Scrips/Scriptable Description.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "ScreenText", menuName = "ScriptableObjects/ScreenText", order = 1)]
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string Titulo;
+    public string Descripcion;
+    public string Codigo;
+}
