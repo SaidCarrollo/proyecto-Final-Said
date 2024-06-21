422c1b67ed9fe9c96f910c683dbe5bdb71e9db8e
SaidCarrollo <150976435+SaidCarrollo@users.noreply.github.com>
2024-06-21 01:41:10 -0400

    Avance Ui
    Avance Ui

--- Proyecto F Said/Assets/Scrips/Butom Manager.cs
+++ Proyecto F Said/Assets/Scrips/Butom Manager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using Cinemachine;
 public class ButomManager : MonoBehaviour
 {
     [SerializeField] private GameObject Bad1;
     [SerializeField] private GameObject God1;
     public string SceneName;
+    private CinemachineVirtualCamera CineMachineVirtualCamera;
+    private CinemachineBasicMultiChannelPerlin CinemachinePerlin;
+    private float Timemovement;
+    private float TImeTotal;
+    private float intesityFist;
     public void Cerrar()
     {
         Bad1.SetActive(false);
@@ -16,10 +21,42 @@ public class ButomManager : MonoBehaviour
     {
         God1.SetActive(false);
     }
+    private void Awake()
+    {
+        CineMachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        CinemachinePerlin = CineMachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
     public void Gameplay()
     {
-        SceneManager.LoadScene("Gameplay");
+        StartCoroutine(PlayGameplayWithEffect());
     }
 
+    private IEnumerator PlayGameplayWithEffect()
+    {
+        MovementCamera(1.0f, 2.0f, 3.0f); 
 
+        yield return new WaitForSeconds(3.0f);
+
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void AlMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void MovementCamera(float intesity, float Frecuenia, float Tiempo)
+    {
+        CinemachinePerlin.m_AmplitudeGain = intesity;
+        CinemachinePerlin.m_AmplitudeGain = Frecuenia;
+        intesityFist = intesity;
+        TImeTotal = Tiempo;
+        Timemovement = Tiempo;
+    }
+    private void Update()
+    {
+        if (Timemovement > 0)
+        {
+            Timemovement -= Time.deltaTime;
+            CinemachinePerlin.m_AmplitudeGain = Mathf.Lerp(intesityFist, 0, 1 - (Timemovement / TImeTotal));
+        }
+    }
 }
--- Proyecto F Said/Assets/Scrips/Menu.cs
+++ Proyecto F Said/Assets/Scrips/Menu.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject BotonPausa;
     [SerializeField] private GameObject BotonDespausar;
-
+    public Volume GlobalVolume;
+    public Vignette viñeta;
     private bool estaEnPausa = false;
+    private void Awake()
+    {
+        viñeta = (Vignette)GlobalVolume.profile.components.Find(c => c is Vignette);
+    }
     public void Pausa(InputAction.CallbackContext contexto)
     {
         if (contexto.performed)
@@ -20,12 +26,14 @@ public class Menu : MonoBehaviour
                 Time.timeScale = 0f;
                 BotonPausa.SetActive(false);
                 BotonDespausar.SetActive(true);
+                viñeta.intensity.value = 0.843f;
             }
             else
             {
                 Time.timeScale = 1f;
                 BotonPausa.SetActive(true);
                 BotonDespausar.SetActive(false);
+                viñeta.intensity.value = 0f;
             }
         }
     }
